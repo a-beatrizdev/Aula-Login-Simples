6db9ed123bcb5c9e396430383c7d1b229d8cb7a2
Alana <alanabeatriztobias@gmail.com>
2026-05-08 15:00:10 -0300

    criando cookie

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from database import get_db, Usuario
 
 app = FastAPI(title="Sistema de login simples")
 
@@ -29,8 +30,6 @@ def tela_login(request: Request):
         "login.html",
         {"request": request}
     )
-
-#Tela inicial 
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse(
@@ -39,3 +38,87 @@ def home(request: Request):
         {"request": request}
     )
 
+# POST - CRIA UM USUÁRIO
+@app.post("/cadastro")
+def cadastrar_usuario(
+    request: Request,
+    email: str = Form(...),
+    senha: str = Form(...),
+    db: Session = Depends(get_db) # Dependência com a tabela 
+):
+
+    #Procurar o email no banco de dados.
+    user_existente = db.query(Usuario).filter_by(email=email).first()
+    # user_existente = db.query(Usuario).filter(Usuario.email == email).first()
+
+    if user_existente:
+        return templates.TemplateResponse(
+            request,
+            "cadastro.html",
+            {"request": request, "erro": "Email já cadastrado."}
+        )
+
+    # Cria um objeto 
+    novo_usuario = Usuario(email=email, senha=senha)
+    db.add(novo_usuario)
+    db.commit()
+
+    return RedirectResponse(url="/login", status_code=303)
+
+#Rota para fazer login
+@app.post("/login")
+def fazer_login(
+    request: Request,
+    email: str = Form(...),
+    senha: str = Form(...),
+    db: Session = Depends(get_db)
+):
+
+    #Verificar login
+    usuario = db.query(Usuario).filter(Usuario.email == email).filter(Usuario.senha == senha).first()
+
+    if usuario is None:
+        return templates.TemplateResponse(
+            request,
+            "login.html",
+            {"request": request, "erro": "Email ou senha inválidos."}
+        )
+
+    response = RedirectResponse(url="/poslogin", status_code=303)
+
+    #Criando o cookie simples
+    response.set_cookie(
+        key="usuario_id",
+        value=str(usuario.id)
+    )
+
+    return response
+
+#Tela protegida
+@app.get("/poslogin")
+def tela_poslogin(request: Request, db: Session = Depends(get_db)):
+
+    #Verificar id no cookie
+    usuario_id = request.cookies.get("usuario_id")
+
+    if usuario_id is None:
+        return RedirectResponse(url="/login", status_code=303)
+
+    user_existente = db.query(Usuario).get(int(usuario_id))
+
+    return templates.TemplateResponse(
+        request,
+        "inicio.html",
+        {"request": request, "usuario": user_existente}
+    )
+
+
+
+#Logout do sistema - Sair
+@app.get("/logout")
+def logout():
+
+    response = RedirectResponse(url="/", status_code=303)
+    response.delete_cookie(usuario_id)
+
+    return response
